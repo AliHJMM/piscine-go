c469520d29c7437b044897aa3adabaf06245badd
AliHJMM <AliHasanJMM@gmail.com>
2024-02-24 18:35:00 +0300

    recurs

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,15 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	
-	if nb < 0{
+
+	if nb < 0 {
 		return 0
-	} else if nb == 0{
+	} else if nb == 0 {
 		return 1
 	}
 
 	result := nb - 1
-	for i:= result; i >= 1; i--{
+	for i := result; i >= 1; i-- {
 		nb *= i
 	}
 	return nb
