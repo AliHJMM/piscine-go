c3e95bebc556f6258b9bd8626d877609dc3e47c7
AliHJMM <AliHasanJMM@gmail.com>
2024-02-24 19:17:28 +0300

    recurs

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,6 +1,5 @@
 package piscine
 
-import "fmt"
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
@@ -12,6 +11,5 @@ func IterativeFactorial(nb int) int {
 	for i := 1; i <= nb; i++ {
 		result *= i
 	}
-	fmt.Print("\n")
 	return result
 }
