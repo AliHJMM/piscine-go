85393f93fe4f76acf95dc910f2cc1c9ac911c8d4
AliHJMM <AliHasanJMM@gmail.com>
2024-02-24 21:05:10 +0300

    recurs

--- isprime.go
+++ isprime.go
@@ -1,14 +1,14 @@
 package piscine
 
-func IsPrime(nbr int) bool {
+func IsPrime(nb int) bool {
 	rep := true
-	if nbr == 2 || nbr == 3 || nbr == 5 || nbr == 7 || nbr == 11 || nbr == 13 || nbr == 17 || nbr == 19 || nbr == 21 || nbr == 23 || nbr == 29 || nbr == 31 || nbr == 37 {
+	if nb == 2 || nb == 3 || nb == 5 || nb == 7 || nb == 11 || nb == 13 || nb == 17 || nb == 19 || nb == 21 || nb == 23 || nb == 29 || nb == 31 || nb == 37 {
 		rep = true
-	} else if nbr == 1 || nbr == 0 {
+	} else if nb == 1 || nb == 0 {
 		rep = false
 	} else {
 		for i := 2; i < 40; i++ {
-			if nbr%i == 0 {
+			if nb%i == 0 {
 				rep = false
 			}
 		}
