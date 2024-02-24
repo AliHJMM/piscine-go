23af9524e6aaee08ff52ef93607c47ddf8a1bcdd
AliHJMM <AliHasanJMM@gmail.com>
2024-02-24 22:38:54 +0300

    recurs

--- findnextprime.go
+++ findnextprime.go
@@ -1,13 +1,10 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if IsPrime(nb) {
-		return nb
-	} else {
-		n := nb + 1
-		for IsPrime(n) == false {
-			n++
+	for a := nb; a >= nb; a++ {
+		if IsPrime(a) {
+			return a
 		}
-		return n
 	}
+	return 1
 }
