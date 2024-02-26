956bf82d80837705f335a015588c7df7ac0e74bb
alihasan6 <AliHasanJMM@gmail.com>
2024-02-26 15:52:47 +0300

    nrune

--- nrune.go
+++ nrune.go
@@ -2,5 +2,13 @@ package piscine
 
 func NRune(s string, n int) rune {
 	slice := []rune(s)
-	return slice[n-1]
+	length := 0
+	for i := range slice {
+		length = i
+	}
+
+	if n-1 >= 0 && n-1 <= length {
+		return slice[n-1]
+	}
+	return 0
 }
