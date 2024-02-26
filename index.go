4dcba174111cac9afbeeaa9225672ec7f4af2de1
alihasan6 <AliHasanJMM@gmail.com>
2024-02-26 16:55:24 +0300

    index

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ func Index(s string, toFind string) int {
 	for i := 0; i <= len(s); i++ {
 		if i+len(toFind) <= len(s) {
 			if s[i:(i+len(toFind))] == toFind {
-				return 1
+				return i
 			}
 		}
 	}
