99bb5360cce89e973c2f813a0e9073ac0012f84a
alihasan6 <AliHasanJMM@gmail.com>
2024-02-26 16:49:51 +0300

    index

---
+++ index.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	for i := 0; i <= len(s); i++ {
+		if i+len(toFind) <= len(s) {
+			if s[i:(i+len(toFind))] == toFind {
+				return 1
+			}
+		}
+	}
+	return -1
+}
