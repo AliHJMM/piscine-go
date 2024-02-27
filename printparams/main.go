82dd85b8164eabd71353c9c1018c63a336d67876
alihasan6 <AliHasanJMM@gmail.com>
2024-02-27 14:11:56 +0300

    para

--- printparams/main.go
+++ printparams/main.go
@@ -8,13 +8,14 @@ import (
 
 func main() {
 	args := os.Args
-
 	for i := range args {
 		if i > 0 {
 			for _, chr := range args[i] {
 				z01.PrintRune(chr)
 			}
 		}
-		z01.PrintRune('\n')
+		if i > 0 {
+			z01.PrintRune('\n')
+		}
 	}
 }
