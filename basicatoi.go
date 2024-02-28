f6c77d9791049aa2ea5cf1095113ef9d6dc112d7
AliHJMM <AliHasanJMM@gmail.com>
2024-02-28 21:56:21 +0300

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BasicAtoi(s string) int {
+
+	slice := []rune(s)
+	ans := 0
+	for i := 0; i < len(s); i++ {
+		if slice[i] < '0' || slice[i] > '9' {
+			return ans
+		} else {
+			ans *= 10
+			ans += int(slice[i]) - '0'
+		}
+	}
+	return ans
+}
