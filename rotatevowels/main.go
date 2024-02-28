797e78f9bb8f67cd339bf049be5d23f2824849a0
alihasan6 <AliHasanJMM@gmail.com>
2024-02-28 12:14:34 +0300

    rotate

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -6,41 +6,41 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	var vowels []rune
-	arguments := os.Args[1:]
-	for _, arg := range arguments {
-		for i := 0; i < len(arg); i++ {
-			if isVowel(rune(arg[i])) {
-				vowels = append(vowels, rune(arg[i]))
-			}
-		}
-	}
-	answer := ""
-	n := len(vowels) - 1
-	for _, word := range arguments {
-		for i := 0; i < len(word); i++ {
-			if isVowel(rune(word[i])) {
-				answer += string(vowels[n])
-				n--
-			} else {
-				answer += string(word[i])
-			}
-		}
-		answer += " "
-	}
-	for _, j := range answer {
-		z01.PrintRune(rune(j))
-	}
-	z01.PrintRune('\n')
-}
-
-func isVowel(letter rune) bool {
-	vowels := "aeiouAEIOU"
-	for _, v1 := range vowels {
-		if letter == v1 {
-			return true
-		}
+func check(x rune) bool {
+	if x == 'a' || x == 'A' || x == 'e' || x == 'E' || x == 'o' || x == 'O' || x == 'u' || x == 'U' || x == 'i' || x == 'I' {
+		return true
 	}
 	return false
+}
+
+func main() {
+	arg := os.Args[1:]
+	rep := []rune{}
+	ans := ""
+	ln := 0
+	IsF := true
+	for _, c := range arg {
+		for _, j := range c {
+			if check(j) {
+				rep = append(rep, j)
+				ln++
+			}
+		}
+		if IsF {
+			ans = c
+			IsF = false
+			continue
+		}
+		ans = ans + " " + c
+	}
+	cur := 0
+	for _, c := range ans {
+		if check(c) {
+			z01.PrintRune(rep[ln-cur-1])
+			cur++
+		} else {
+			z01.PrintRune(c)
+		}
+	}
+	z01.PrintRune('\n')
 }
