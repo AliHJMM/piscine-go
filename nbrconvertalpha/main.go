edef489b8c606308a3ad3c8bab312bfde6ee2385
alihasan6 <AliHasanJMM@gmail.com>
2024-02-28 11:52:22 +0300

    convertalpha

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -6,53 +6,53 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func BasicAtoi(s string) int {
-	x := 0
-	for _, cifra := range s {
-		if '0' <= cifra && cifra <= '9' {
-			z := 0
-			for k := '1'; k <= cifra; k++ {
-				z++
-			}
-			x = x*10 + z
-		} else {
-			x = -1
-			break
-		}
-	}
-	if (x != -1) && !(1 <= x && x <= 26) {
-		x = -1
-	}
-	return x
-}
-
 func main() {
-	arg := os.Args
-	pos := 1
-	flagupper := false
-	c := 0
-	for range arg {
-		c++
-	}
-	if c >= 2 && arg[1] == "--upper" {
-		pos = 1
-		flagupper = true
-	}
-	for index, k := range arg {
-		if index >= pos {
-			num := BasicAtoi(k)
-			if num == -1 {
-				z01.PrintRune(' ')
-			} else {
-				if !flagupper {
-					z01.PrintRune(rune('a' + num - 1))
-					// z01.PrintRune(rune('a' + num -32))
+	if len(os.Args) > 1 {
+		arguments := os.Args[1:]
+		arrayNumbers := []string{}
+		arrayLetters := []rune{}
+		for i := 1; i < 27; i++ {
+			if i < 10 {
+				arrayNumbers = append(arrayNumbers, string(rune(i+'0')))
+			}
+			if i >= 10 {
+				mod := i % 10
+				decimal := i / 10
+				arrayNumbers = append(arrayNumbers, (string(rune(decimal+'0')) + string(rune(mod+'0'))))
+			}
+		}
+		if arguments[0] != "--upper" {
+			for i := 'a'; i <= 'z'; i++ {
+				arrayLetters = append(arrayLetters, i)
+			}
+		}
+		if arguments[0] == "--upper" {
+			for i := 'A'; i <= 'Z'; i++ {
+				arrayLetters = append(arrayLetters, i)
+			}
+		}
+		for i := 0; i < len(arguments); i++ {
+			if arguments[i] != "--upper" {
+				if inArray(arguments[i], arrayNumbers) {
+					for index, value := range arrayNumbers {
+						if arguments[i] == value {
+							z01.PrintRune(arrayLetters[index])
+						}
+					}
 				} else {
-					z01.PrintRune(rune('A' + num - 1))
-					// z01.PrintRune(rune('A' + num - 32))
+					z01.PrintRune(' ')
 				}
 			}
 		}
+		z01.PrintRune('\n')
 	}
-	z01.PrintRune(10)
+}
+
+func inArray(a string, b []string) bool {
+	for _, value := range b {
+		if a == value {
+			return true
+		}
+	}
+	return false
 }
