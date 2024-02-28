85c625ae6531aeff04d1ab1c8a6aface5defbbc3
AliHJMM <AliHasanJMM@gmail.com>
2024-02-28 19:20:03 +0300

    nbr

--- printnbr.go
+++ printnbr.go
@@ -11,14 +11,13 @@ func PrintNbr(n int) {
 		z01.PrintRune('-')
 	}
 	if n != 0 {
-		f := (n/10) * t
+		f := (n / 10) * t
 		if f != 0 {
 			PrintNbr(f)
 		}
 		k := (n % 10 * t) + '0'
 		z01.PrintRune(rune(k))
-		} else {
-			z01.PrintRune('0')
+	} else {
+		      z01.PrintRune('0')
 	}
-		
 }
