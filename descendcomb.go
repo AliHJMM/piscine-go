facfe263a1788f6ce18f8f36137c2175b04e2b94
AliHJMM <AliHasanJMM@gmail.com>
2024-03-05 18:28:31 +0300

    descend

--- descendcomb.go
+++ descendcomb.go
@@ -1,18 +1,25 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func DescendComb() {
-	for i := 9; i >= 0; i-- {
-		for j := 9; j >= 0; j-- {
-			z01.PrintRune(rune('0' + i))
-			z01.PrintRune(rune('0' + j))
+	for i := 99; i >= 1; i-- {
+		for j := 98; j >= 0; j-- {
+			z01.PrintRune('0' + rune(i/10))
+			z01.PrintRune('0' + rune(i%10))
+			z01.PrintRune(' ')
 
-			if i != 0 || j != 0 {
+			z01.PrintRune('0' + rune(j/10))
+			z01.PrintRune('0' + rune(j%10))
+
+			if j > 0 || i > 1 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
+			} else {
+				z01.PrintRune('\n')
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
