70fd6af147ad56be4cbfb76289b7915239dda201
AliHJMM <AliHasanJMM@gmail.com>
2024-03-05 23:33:24 +0300

    food

---
+++ fooddeliverytime.go
@@ -0,0 +1,23 @@
+package piscine
+
+import (
+	"fmt"
+)
+
+type food struct {
+	preptime int
+}
+
+var menu = map[string]food{
+	"burger":  {preptime: 15},
+	"chips":   {preptime: 10},
+	"nuggets": {preptime: 12},
+}
+
+func FoodDeliveryTime(order string) (int, error) {
+	item, exists := menu[order]
+	if !exists {
+		return 0, fmt.Errorf("404: Item not found in the menu")
+	}
+	return item.preptime, nil
+}
