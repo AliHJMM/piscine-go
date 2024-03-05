fa1d9d0baf3085aa2933214f433a479059c2443f
AliHJMM <AliHasanJMM@gmail.com>
2024-03-05 21:17:42 +0300

    comcheck

--- comcheck/main.go
+++ comcheck/main.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 	"os"
 )
-
 func main() {
 	args := os.Args[1:]
 	for _, chr := range args {
 		if chr == "01" || chr == "galaxy" || chr == "galaxy 01" {
-			fmt.Print("Alert!!")
-		} else {
-			return
-		} 
-	}
-	fmt.Print("\n")
-}
+			fmt.Println("Alert!!!")
+			} else {
+				return
+				} 
+			}
+		}
