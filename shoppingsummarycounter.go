f0c4e0c1467dc03ad175aa996c60c5d879d4a79d
AliHJMM <AliHasanJMM@gmail.com>
2024-03-05 23:16:35 +0300

    counter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -2,24 +2,15 @@ package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
 	summary := make(map[string]int)
-	wordStart := -1
-
-	for i, c := range str {
-		isSpace := c == ' ' || c == '\t' || c == '\n' || c == '\r'
-		if wordStart == -1 && !isSpace {
-			wordStart = i
-		} else if wordStart != -1 && (isSpace || i == len(str)-1) {
-			if isSpace {
-				word := str[wordStart:i]
-				if word != "" {
-					summary[word]++
-				}
-			} else {
-				summary[str[wordStart:]]++
-			}
-			wordStart = -1
+	item := ""
+	for _, char := range str {
+		if char == ' ' {
+			summary[item]++
+			item = ""
+		} else {
+			item += string(char)
 		}
 	}
-
+	summary[item]++
 	return summary
 }
