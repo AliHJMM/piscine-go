112e91ac78411fcb7e17f1f5b9705e5d25a2a9f3
AliHJMM <AliHasanJMM@gmail.com>
2024-03-05 22:23:54 +0300

    pilot

--- pilot/main.go
+++ pilot/main.go
@@ -1,10 +1,26 @@
-package piscine
+package main
+
+import "fmt"
+
+type AircraftType int
+
+const (
+	AIRCRAFT1 AircraftType = 1
+)
 
 type Pilot struct {
 	Name     string
-	Life     int64
+	Life     float64
 	Age      int
-	Aircraft int
+	Aircraft AircraftType
 }
 
-const AIRCRAFT1 int = 1
+func main() {
+	var donnie Pilot
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = AIRCRAFT1
+
+	fmt.Println(donnie)
+}
