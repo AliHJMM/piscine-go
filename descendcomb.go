aab5a1b217094f625016c430d60d321b7541e7fb
AliHJMM <AliHasanJMM@gmail.com>
2024-03-06 23:17:53 +0300

    comb

--- descendcomb.go
+++ descendcomb.go
@@ -6,7 +6,7 @@ import (
 
 func DescendComb() {
 	for i := 99; i >= 1; i-- {
-		for j := i-1; j >= 0; j-- {
+		for j := i - 1; j >= 0; j-- {
 			z01.PrintRune('0' + rune(i/10))
 			z01.PrintRune('0' + rune(i%10))
 			z01.PrintRune(' ')
@@ -14,7 +14,7 @@ func DescendComb() {
 			z01.PrintRune('0' + rune(j/10))
 			z01.PrintRune('0' + rune(j%10))
 
-			if !(i==1 && j == 0) {
+			if !(i == 1 && j == 0) {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
