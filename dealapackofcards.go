394122074a58358993c078f59aa3fc4e5d200ea1
AliHJMM <AliHasanJMM@gmail.com>
2024-03-06 22:54:55 +0300

    deal

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,44 +1,18 @@
 package piscine
 
-import (
-	"fmt"
-
-	"github.com/01-edu/z01"
-)
-
+import "fmt"
 func DealAPackOfCards(deck []int) {
-	playersNumber := 4
-	cards := len(deck) / playersNumber
-
-	for i := 0; i < playersNumber; i++ {
-		start := i * cards
-		end := (i + 1) * cards
-		playerCards := deck[start:end]
-
-		fmt.Printf("Player %d: ", i+1)
-		for _, card := range playerCards {
-			printIntAsRunes(card)
-			z01.PrintRune(' ')
+	player := 1
+	for j := 0; j < 12; {
+		fmt.Printf("Player %d: ", player)
+		for i := 1; i < 4; i++ {
+			fmt.Printf("%d", deck[j])
+			j++
+			if i != 3 {
+				fmt.Printf(", ")
+			}
 		}
-		z01.PrintRune('\n')
-	}
-}
-
-func printIntAsRunes(num int) {
-	if num == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
-	var digits []rune
-
-	for num > 0 {
-		digit := num % 10
-		digits = append(digits, rune(digit+'0'))
-		num /= 10
-	}
-
-	for i := len(digits) - 1; i >= 0; i-- {
-		z01.PrintRune(digits[i])
+		player++
+		fmt.Printf("\n")
 	}
 }
